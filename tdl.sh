408f5daafe6fbea7e18059ccaf8e0a2a8a3e2ca5
Lucccyo <cha.git@mailo.fr>
2023-09-22 12:02:36 +0200

    open all windows and quit the first terminal

--- tdl.sh
+++ tdl.sh
@@ -1,24 +1,28 @@
 #!/bin/bash 
 
-# -c -l -o -r 
-
 project_dir="$HOME/.config/tdl/"
 
 if [[ "$#" -eq 0 ]]; then 
   exit 0 
 fi
 if [[ "$1" =~ ^- ]]; then
-  echo "c'est une option — soit list ; soit create NAME"
   if [[ ! (($1 == "-c" || $1 == "-o" || $1 == "-r") && -n $2) && ! $1 == "-l" ]]; then
     echo 'invalid command'
     exit 0
   elif [[ $1 == '-l' ]]; then
-    echo 'on liste les projects'
+    ls $project_dir | grep -v $project_dir
   else 
     case "$1" in
-      '-c' ) echo 'on crée un projet' ;;
-      '-o' ) echo 'on ouvre un projet' ;;
-      '-r' ) echo 'on suprimme un projet';;
+      '-c' ) 
+	mkdir "$project_dir$2"
+	touch "$project_dir$2/paths.sh"
+	echo '#!/bin/bash' > "$project_dir$2/paths.sh";;
+      '-o' ) 
+	setsid sh "$project_dir$2/paths.sh" &
+	kill -9 $(ps -o ppid= -p $$)
+	;;
+      '-r' ) 
+	rm -rf "$project_dir$2";;
     esac 
   fi
 else 
@@ -29,16 +33,32 @@ else
     echo 'invalid command'
     exit 0
   elif [[ $2 == '-l' ]]; then
-    echo "on liste les paths de $1"
+    grep -v '^\s*$\|^#' "$project_dir$1/paths.sh"
   else 
     case "$2" in
-      '-a' ) echo "on ajoute les paths ... a $1" ;;
+      '-a' ) 
+	if [[ "$#" -lt 5 ]]; then
+	  exit 0
+	else
+	  echo "#$3" >> "$project_dir$1/paths.sh"
+	  case "$4" in
+  	    '-f' ) echo "firefox -new-window $5 &" >> "$project_dir$1/paths.sh" ;;
+  	    '-v' ) echo "alacritty -e nvim $5 &" >> "$project_dir$1/paths.sh" ;;
+  	    '-t' ) echo "alacritty --working-directory $5 &" >> "$project_dir$1/paths.sh" ;;
+	  esac
+	fi
+
+	;;
       '-g' ) echo "on ghost les paths ... dans $1" ;;
       '-r' )
 	if [[ $3 == "-all" ]]; then
-	  echo "on remove tout les paths dans $1"
+	  echo "#!/bin/bash" > "$project_dir$1/paths.sh"
 	else
-	  echo "on remove les paths ... dans $1" 
+	  l=$(grep -n "^#$3" "$project_dir$1/paths.sh" | head -c 1)
+	  if [[ $l -gt 1 ]]; then
+	    sed -i "${l}d" "$project_dir$1/paths.sh"
+	    sed -i "${l}d" "$project_dir$1/paths.sh"
+	  fi
 	fi ;;
     esac
   fi
